3e19f00c43fdd94a2d1c285b71b2d2b48a27f2db
Joan Fernández Navarro <joanfernandeznv@gmail.com>
2024-06-24 00:14:22 +0200

    Error page

--- pages/error.py
+++ pages/error.py
@@ -1,12 +1,16 @@
-from dash import html
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 from .navbar import create_navbar
 
 nav = create_navbar()
-header = html.H3("ERROR PAGE!!")
 
 def create_page_error():
     layout = html.Div([
         nav,
-        header
+        html.Div([
+            html.H1("404 - Page Not Found"),
+            html.P("Sorry, the page you are looking for does not exist."),
+            dcc.Link(dbc.Button("Back to Home", color = "primary"), href = "/")
+        ], style={"textAlign": "center", "marginTop": "20%"})
     ])
     return layout
